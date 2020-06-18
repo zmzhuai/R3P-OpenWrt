1a5871d8b2298c7ae93018186e0e689fcf3003a1
Min <zmzhuai@gmail.com>
2020-06-18 15:42:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+sed -i "s/enabled = true/enabled = false/g" package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/genv2config.lua
